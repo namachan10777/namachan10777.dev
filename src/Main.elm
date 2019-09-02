4202f14e0c95f683b66dba112dc949433a81c785
namachan10777 <namachan10777@gmail.com>
2019-09-02 16:25:07 +0900

    add footer

--- src/Main.elm
+++ src/Main.elm
@@ -3,9 +3,11 @@ port module Main exposing (Model)
 import Browser
 import Browser.Events
 import Html exposing (..)
-import Html.Attributes exposing (class, src, id, href)
+import Html.Attributes exposing (class, href, id, src)
 import Html.Events exposing (..)
 import Json.Decode as Decode
+import Task
+import Time
 import Types exposing (KeyValue)
 import Util
 
@@ -29,20 +31,31 @@ type Hist
 type alias Model =
     { current : String
     , hists : List Hist
+    , posix : Time.Posix
+    , zone : Time.Zone
     }
 
 
 type Msg
     = Type KeyValue
     | ScrollBottom
+    | SetSystemTime ( Time.Zone, Time.Posix )
+    | SetCurrentTime Time.Posix
+
+
+setSystemTime : Cmd Msg
+setSystemTime =
+    Task.perform SetSystemTime <| Task.map2 Tuple.pair Time.here Time.now
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( { current = "help"
       , hists = []
+      , posix = Time.millisToPosix 0
+      , zone = Time.utc
       }
-    , Cmd.none
+    , setSystemTime
     )
 
 
@@ -97,7 +110,9 @@ analyzeCurrent current =
 
 -- UPDATE
 
-port scrollBottom: () -> Cmd msg
+
+port scrollBottom : () -> Cmd msg
+
 
 decode : String -> Hist
 decode acc =
@@ -139,7 +154,14 @@ update msg model =
             ( model, Cmd.none )
 
         ScrollBottom ->
-            ( model,  scrollBottom ())
+            ( model, scrollBottom () )
+
+        SetSystemTime ( zone, time ) ->
+            ( { model | posix = time, zone = zone }, Cmd.none )
+
+        SetCurrentTime time ->
+            ( { model | posix = time }, Cmd.none )
+
 
 
 -- VIEW
@@ -164,6 +186,71 @@ renderCurrent current =
             span [ class "complete" ] [ text typed ]
 
 
+toEnMonth month =
+    case month of
+        Time.Jan ->
+            "Jan"
+
+        Time.Feb ->
+            "Feb"
+
+        Time.Mar ->
+            "Mar"
+
+        Time.Apr ->
+            "Apr"
+
+        Time.May ->
+            "May"
+
+        Time.Jun ->
+            "Jun"
+
+        Time.Jul ->
+            "Jul"
+
+        Time.Aug ->
+            "Aug"
+
+        Time.Sep ->
+            "Sep"
+
+        Time.Oct ->
+            "Oct"
+
+        Time.Nov ->
+            "Nov"
+
+        Time.Dec ->
+            "Dec"
+
+
+renderPowerline model =
+    let
+        year =
+            Time.toYear model.zone model.posix |> String.fromInt
+
+        month =
+            Time.toMonth model.zone model.posix |> toEnMonth
+
+        day =
+            Time.toDay model.zone model.posix |> String.fromInt
+
+        hour =
+            Time.toHour model.zone model.posix |> String.fromInt
+
+        minute =
+            Time.toMinute model.zone model.posix |> String.fromInt
+
+        second =
+            Time.toSecond model.zone model.posix |> String.fromInt
+    in
+    footer []
+        [ span [ class "terminal-info" ] [ text "fish- 1:namachan10777*" ]
+        , span [ class "time" ] [ text (month ++ " " ++ day ++ " " ++ year ++ " " ++ hour ++ ":" ++ minute ++ ":" ++ second) ]
+        ]
+
+
 renderHists hists =
     List.map
         (\hist ->
@@ -184,9 +271,9 @@ renderHists hists =
                     div []
                         [ div [ class "complete" ] [ renderPrompt, text s ]
                         , img [ src "./res/icon.jpg" ] [ renderPrompt ]
-                        , div [] [
-                                span [] [ text "by " ],
-                                a [ href "https://twitter.com/hsm_hx" ] [ text "@hsm_hx" ]
+                        , div []
+                            [ span [] [ text "by " ]
+                            , a [ href "https://twitter.com/hsm_hx" ] [ text "@hsm_hx" ]
                             ]
                         ]
 
@@ -202,7 +289,9 @@ view model =
         lists =
             div [] [ renderPrompt, renderCurrent model.current ] :: renderHists model.hists
     in
-    div [ id "root" ] (List.reverse lists)
+    div [ id "root" ] [
+        div [ id "scroll-area" ] (List.reverse lists)
+        , renderPowerline model ]
 
 
 
@@ -213,4 +302,5 @@ subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.batch
         [ Browser.Events.onKeyDown (Decode.map Type Util.keyDecoder)
+        , Time.every 1000 SetCurrentTime
         ]
