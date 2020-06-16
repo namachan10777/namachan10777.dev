09db9e716441b4d2237000a51df1e39cc1f8d856
Nakano Masaki <namachan10777@gmail.com>
2020-06-16 22:03:00 +0900

    full parser

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -10,4 +10,18 @@ pub enum Value {
     Int(i64),
     Float(f64),
     Str(String),
+    Text(Vec<TextElem>),
+}
+
+#[derive(PartialEq, Debug)]
+pub struct Cmd {
+    name: String,
+    attrs: Vec<(String, Value)>,
+    inner: Vec<TextElem>,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum TextElem {
+    Cmd(Cmd),
+    Plain(String),
 }
--- engine/src/parser.rs
+++ engine/src/parser.rs
@@ -1,4 +1,4 @@
-use super::Value;
+use super::{Cmd, TextElem, Value};
 use pest::iterators::{Pair, Pairs};
 use pest::Parser;
 
@@ -9,6 +9,72 @@ struct TextParser;
 #[derive(Debug, PartialEq)]
 enum Error {
     InternalError(String),
+}
+
+fn parse_cmd(pair: Pair<Rule>) -> Cmd {
+    match pair.as_rule() {
+        Rule::cmd => {
+            let mut inner = pair.into_inner();
+            let name = &inner.next().unwrap().as_str()[1..];
+            let attrs = inner
+                .next()
+                .unwrap()
+                .into_inner()
+                .map(|p| {
+                    let mut inner = p.into_inner();
+                    let attr = inner.next().unwrap().as_str();
+                    let value = parse_value(inner.next().unwrap().into_inner().next().unwrap());
+                    (attr.to_owned(), value)
+                })
+                .collect::<Vec<_>>();
+            let inner = inner.next().unwrap();
+            let cmd_inner = match inner.as_rule() {
+                Rule::text => fold_textelem(inner.into_inner()),
+                Rule::end_of_cmd => vec![],
+                _ => unreachable!(),
+            };
+            Cmd {
+                name: name.to_owned(),
+                attrs,
+                inner: cmd_inner,
+            }
+        }
+        _ => unreachable!(),
+    }
+}
+
+fn parse_text_elem(pair: Pair<Rule>) -> TextElem {
+    match pair.as_rule() {
+        Rule::cmd => TextElem::Cmd(parse_cmd(pair)),
+        Rule::esc_endbrace => TextElem::Plain(String::from("}")),
+        Rule::esc_esc => TextElem::Plain(String::from("\\")),
+        Rule::char_in_text => TextElem::Plain(pair.as_str().to_owned()),
+        _ => unreachable!(),
+    }
+}
+
+fn fold_textelem(pairs: Pairs<Rule>) -> Vec<TextElem> {
+    let mut inner = Vec::new();
+    let mut text = String::new();
+    for elem in pairs.map(parse_text_elem) {
+        match elem {
+            TextElem::Plain(s) => {
+                text.push_str(s.as_str());
+            }
+            TextElem::Cmd(cmd) => {
+                if text.len() > 0 {
+                    inner.push(TextElem::Plain(text.clone()));
+                }
+                inner.push(TextElem::Cmd(cmd));
+                text.clear();
+            }
+        }
+    }
+    if text.len() > 0 {
+        inner.push(TextElem::Plain(text));
+    }
+    println!("{:?}", inner);
+    inner
 }
 
 fn parse_value(pair: Pair<Rule>) -> Value {
@@ -27,7 +93,8 @@ fn parse_value(pair: Pair<Rule>) -> Value {
                 .collect::<Vec<_>>();
             Value::Str(inner.join(""))
         }
-        _ => unreachable!(),
+        Rule::text => Value::Text(fold_textelem(pair.into_inner())),
+        p => unreachable!(),
     }
 }
 
@@ -42,7 +109,7 @@ mod test {
     }
 
     #[test]
-    fn parse_int() {
+    fn test_int() {
         assert_eq!(
             parse(Rule::int, "1234", parse_value),
             Ok(Some(Value::Int(1234))),
@@ -53,9 +120,8 @@ mod test {
         );
     }
 
-
     #[test]
-    fn parse_float() {
+    fn test_float() {
         assert_eq!(
             parse(Rule::float, "3.14", parse_value),
             Ok(Some(Value::Float(3.14))),
@@ -67,7 +133,7 @@ mod test {
     }
 
     #[test]
-    fn parse_str() {
+    fn test_str() {
         assert_eq!(
             parse(Rule::str, "\"abc\"", parse_value),
             Ok(Some(Value::Str("abc".to_owned()))),
@@ -75,6 +141,82 @@ mod test {
         assert_eq!(
             parse(Rule::str, "\"abc\\\"def\\\\\"", parse_value),
             Ok(Some(Value::Str("abc\"def\\".to_owned()))),
+        );
+    }
+
+    #[test]
+    fn test_cmd() {
+        assert_eq!(
+            parse(Rule::cmd, "\\cmdname class=\"cls1\";", parse_cmd),
+            Ok(Some(Cmd {
+                name: "cmdname".to_owned(),
+                attrs: vec![("class".to_owned(), Value::Str("cls1".to_owned()))],
+                inner: vec![]
+            }))
+        );
+        println!(
+            "{:#?}",
+            parse(
+                Rule::cmd,
+                "\\cmdname class=\"cls1\"{\\c1; \\c2 {\\c3;}}",
+                parse_cmd
+            )
+        );
+        assert_eq!(
+            parse(
+                Rule::cmd,
+                "\\cmdname class=\"cls1\"{\\c1; \\c2 {\\c3;}}",
+                parse_cmd
+            ),
+            Ok(Some(Cmd {
+                name: "cmdname".to_owned(),
+                attrs: vec![("class".to_owned(), Value::Str("cls1".to_owned()))],
+                inner: vec![
+                    TextElem::Cmd(Cmd {
+                        name: "c1".to_owned(),
+                        attrs: vec![],
+                        inner: vec![]
+                    }),
+                    TextElem::Plain(" ".to_owned()),
+                    TextElem::Cmd(Cmd {
+                        name: "c2".to_owned(),
+                        attrs: vec![],
+                        inner: vec![TextElem::Cmd(Cmd {
+                            name: "c3".to_owned(),
+                            attrs: vec![],
+                            inner: vec![]
+                        })]
+                    })
+                ]
+            }))
+        );
+    }
+    #[test]
+    fn test_text() {
+        assert_eq!(
+            parse(Rule::text, "{}", parse_value),
+            Ok(Some(Value::Text(vec![]))),
+        );
+        assert_eq!(
+            parse(Rule::text, "{\\}\\\\}", parse_value),
+            Ok(Some(Value::Text(vec![TextElem::Plain(String::from(
+                "}\\"
+            )),]))),
+        );
+        assert_eq!(
+            parse(Rule::text, "{\\cmd {foo\\red {bar}}}", parse_value),
+            Ok(Some(Value::Text(vec![TextElem::Cmd(Cmd {
+                name: String::from("cmd"),
+                attrs: vec![],
+                inner: vec![
+                    TextElem::Plain(String::from("foo")),
+                    TextElem::Cmd(Cmd {
+                        name: String::from("red"),
+                        attrs: vec![],
+                        inner: vec![TextElem::Plain(String::from("bar"))]
+                    }),
+                ]
+            })]))),
         );
     }
 }
