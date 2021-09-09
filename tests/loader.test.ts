e19401325d62eaba659bc89066587a9211824bce
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-09-09 20:48:16 +0900

    test minimal parser

--- tests/loader.test.ts
+++ tests/loader.test.ts
@@ -7,3 +7,26 @@ describe('greet', (): void => {
     expect(Lib.hello()).toBe('Hello')
   })
 })
+
+describe('parse', (): void => {
+  const p = {
+    line: 0,
+    col: 0,
+    abs: 0,
+  };
+  test('parse simple command', (): void => {
+    expect(Lib.parse(p, "\\hoge;")).toStrictEqual({
+      success: true,
+      result: {
+        type: 'simple',
+        name: 'hoge',
+        args: [],
+      },
+      next: {
+        abs: 6,
+        col: 6,
+        line: 0,
+      }
+    });
+  })
+})
--- webpack-tml-loader/lib.ts
+++ webpack-tml-loader/lib.ts
@@ -5,6 +5,12 @@ type Arg = {
   text: Text;
 };
 
+type Position = {
+  abs: number;
+  line: number;
+  col: number;
+};
+
 type Command =
   | {
       type: 'simple';
@@ -27,14 +33,12 @@ type Command =
 type ParseResult<T> =
   | {
       success: true;
+      next: Position;
       result: T;
     }
   | {
       success: false;
-      position: {
-        line: number;
-        col: number;
-      };
+      position: Position;
     };
 
 type TextElem =
@@ -47,14 +51,58 @@ type TextElem =
       plaintext: string;
     };
 
-export function parse (_: string): ParseResult<Command> {
-  return {
-    success: false,
-    position: {
-      line: 0,
-      col: 0
+function count_line_and_col(src: string, p_init: Position, n: number): Position {
+  let line = p_init.line;
+  let col = p_init.col;
+  let abs = p_init.abs;
+  for (let i = 0; i < n; ++i) {
+    ++abs;
+    ++col;
+    if (/^\n$/.test(src[i])) {
+      ++line;
+      col = 0;
     }
   }
+  return { abs, line, col };
+}
+
+export function eat_space(p: Position, src: string): ParseResult<null> {
+  const white_space_length = src.match(/[ \n\t\r]*/)?.length;
+  if (white_space_length) {
+    const p_next = count_line_and_col(src, p, white_space_length);
+    return {
+      success: true,
+      next: p_next,
+      result: null,
+    };
+  }
+  else {
+    return {
+      success: false,
+      position: p,
+    };
+  }
+}
+
+export function parse (p: Position, src: string): ParseResult<Command> {
+  const name = /^\\(\w+)\s*/.exec(src);
+  if (name) {
+    if (src.length > name[0].length && src[name[0].length] == ';') {
+      return {
+        success: true,
+        next: count_line_and_col(src, p, name[0].length + 1),
+        result: {
+          type: 'simple',
+          name: name[1],
+          args: [],
+        }
+      };
+    }
+  }
+  return {
+    success: false,
+    position: p,
+  };
 }
 
 export function hello () {
