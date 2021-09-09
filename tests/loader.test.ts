2a0b2b617865b7bcb4204bd4eb1542d335f2aad6
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-09-09 23:29:42 +0900

    TODO: blockquote

--- tests/loader.test.ts
+++ tests/loader.test.ts
@@ -1,51 +1,89 @@
-import 'jest'
+import "jest";
 
-import * as Lib from '../webpack-tml-loader/lib'
+import * as Lib from "../webpack-tml-loader/lib";
 
-describe('greet', (): void => {
-  test('should say hello.', (): void => {
-    expect(Lib.hello()).toBe('Hello')
-  })
-})
+describe("greet", (): void => {
+  test("should say hello.", (): void => {
+    expect(Lib.hello()).toBe("Hello");
+  });
+});
 
-describe('parse', (): void => {
+describe("parse", (): void => {
   const p = {
     line: 0,
     col: 0,
     abs: 0,
   };
-  test('parse simple command', (): void => {
+  test("parse simple command", (): void => {
     expect(Lib.parse(p, "\\hoge;")).toStrictEqual({
       success: true,
       result: {
-        type: 'simple',
-        name: 'hoge',
+        type: "simple",
+        name: "hoge",
         args: [],
       },
       next: {
         abs: 6,
         col: 6,
         line: 0,
-      }
+      },
     });
-  })
-  test('parse simple command with args', (): void => {
-    expect(Lib.parse(p, "\\hoge foo=1 bar=3.14 hoge=\"hoge\";")).toStrictEqual({
+  });
+  test("parse simple command with args", (): void => {
+    expect(Lib.parse(p, '\\hoge foo=1 bar=3.14 hoge="hoge";')).toStrictEqual({
       success: true,
       result: {
-        type: 'simple',
-        name: 'hoge',
+        type: "simple",
+        name: "hoge",
         args: [
-          { name: 'foo', value: { type: 'int', int: 1 }},
-          { name: 'bar', value: { type: 'float', float: 3.14 }},
-          { name: 'hoge', value: { type: 'string', str: "hoge" }}
+          { name: "foo", value: { type: "int", int: 1 } },
+          { name: "bar", value: { type: "float", float: 3.14 } },
+          { name: "hoge", value: { type: "string", str: "hoge" } },
         ],
       },
       next: {
         abs: 33,
         col: 33,
         line: 0,
-      }
+      },
     });
-  })
-})
+  });
+  test("parse with-text command with args", (): void => {
+    expect(Lib.parse(p, "\\hoge foo=1 {oh \\bar;}")).toStrictEqual({
+      success: true,
+      result: {
+        type: "with-text",
+        name: "hoge",
+        args: [{ name: "foo", value: { type: "int", int: 1 } }],
+        text: [
+          { type: "plaintext", plaintext: "oh " },
+          { type: "cmd", cmd: { type: "simple", name: "bar", args: [] } },
+        ],
+      },
+      next: {
+        abs: 31,
+        col: 31,
+        line: 0,
+      },
+    });
+  });
+  test("parse with-cmds command without args", (): void => {
+    expect(Lib.parse(p, "\\hoge [\\bar; \\foo;]")).toStrictEqual({
+      success: true,
+      result: {
+        type: "with-cmds",
+        name: "hoge",
+        args: [],
+        cmds: [
+          { type: "simple", name: "bar", args: [] },
+          { type: "simple", name: "foo", args: [] },
+        ],
+      },
+      next: {
+        abs: 31,
+        col: 31,
+        line: 0,
+      },
+    });
+  });
+});
--- webpack-tml-loader/lib.ts
+++ webpack-tml-loader/lib.ts
@@ -84,9 +84,9 @@ function count_line_and_col(
 }
 
 export function eat_space(p: Position, src: string): Position {
-  const white_space_length = src.match(/[ \n\t\r]*/)?.length;
+  const white_space_length = /^[ \n\t\r]*/.exec(src);
   if (white_space_length) {
-    return count_line_and_col(src, p, white_space_length);
+    return count_line_and_col(src, p, white_space_length[0].length);
   } else {
     return p;
   }
@@ -127,7 +127,7 @@ function parse_value(p: Position, src: string): ParseResult<Arg> {
       if (src[i] == '"') {
         return {
           success: true,
-          next: count_line_and_col(src, p, i),
+          next: count_line_and_col(src, p, i + 1),
           result: {
             type: "string",
             str: src.slice(1, i),
@@ -168,6 +168,78 @@ function parse_arg(
   };
 }
 
+export function parse_text(p: Position, src: string): ParseResult<Text> {
+  let next = p;
+  let elems: TextElem[] = [];
+  let text = "";
+  for (let i = 0; i < src.length; ) {
+    if (/^\\[\\\}]$/.test(src.slice(i))) {
+      i += 2;
+      continue;
+    }
+    if (src[i] == "\\") {
+      if (text) {
+        elems.push({ type: "plaintext", plaintext: text });
+        text = "";
+      }
+      const result = parse(next, src.slice(next.abs - p.abs));
+      if (result.success) {
+        elems.push({
+          type: "cmd",
+          cmd: result.result,
+        });
+        i += result.next.abs - next.abs;
+        next = result.next;
+      } else {
+        return {
+          success: false,
+          position: next,
+        };
+      }
+    } else if (src[i] == "}") {
+      break;
+    } else {
+      text += src[i];
+      next = count_line_and_col(src, next, 1);
+      i += 1;
+    }
+  }
+  if (text) {
+    elems.push({ type: "plaintext", plaintext: text });
+    text = "";
+  }
+  return {
+    success: true,
+    result: elems,
+    next,
+  };
+}
+
+export function parse_cmds(p: Position, src: string): ParseResult<Command[]> {
+  let next = eat_space(p, src);
+  let elems: Command[] = [];
+  for (;;) {
+    if (src[next.abs - p.abs] == "]") {
+      return {
+        success: true,
+        next,
+        result: elems,
+      };
+    }
+    const result = parse(next, src.slice(next.abs - p.abs));
+    if (result.success) {
+      elems.push(result.result);
+      next = eat_space(result.next, src.slice(result.next.abs - p.abs));
+    } else {
+      break;
+    }
+  }
+  return {
+    success: false,
+    position: next,
+  };
+}
+
 export function parse(p: Position, src: string): ParseResult<Command> {
   const name = /^\\(\w+)\s*/.exec(src);
   if (name) {
@@ -177,14 +249,17 @@ export function parse(p: Position, src: string): ParseResult<Command> {
       const arg_result = parse_arg(p_next, src.slice(p_next.abs - p.abs));
       if (!arg_result.success) break;
       args.push(arg_result.result);
-      p_next = eat_space(arg_result.next, src.slice(arg_result.next.abs));
+      p_next = eat_space(
+        arg_result.next,
+        src.slice(arg_result.next.abs - p.abs)
+      );
     }
-    const closing = /^\s*;/.exec(src.slice(p_next.abs));
-    if (closing) {
+    const closing = /^\s*([;\{\[])/.exec(src.slice(p_next.abs - p.abs));
+    if (closing && closing[1] == ";") {
       return {
         success: true,
         next: count_line_and_col(
-          src.slice(p_next.abs),
+          src.slice(p_next.abs - p.abs),
           p_next,
           closing[0].length
         ),
@@ -194,6 +269,48 @@ export function parse(p: Position, src: string): ParseResult<Command> {
           name: name[1],
         },
       };
+    } else if (closing && closing[1] == "{") {
+      const text_result = parse_text(
+        count_line_and_col(src, p_next, closing[0].length),
+        src.slice(p_next.abs - p.abs + closing[0].length)
+      );
+      if (text_result.success && src[text_result.next.abs - p.abs] == "}") {
+        return {
+          success: true,
+          next: count_line_and_col(
+            src.slice(p_next.abs - p.abs),
+            text_result.next,
+            text_result.next.abs - p_next.abs + 1
+          ),
+          result: {
+            type: "with-text",
+            args,
+            name: name[1],
+            text: text_result.result,
+          },
+        };
+      }
+    } else if (closing && closing[1] == "[") {
+      const cmds_result = parse_cmds(
+        count_line_and_col(src, p_next, closing[0].length),
+        src.slice(p_next.abs - p.abs + closing[0].length)
+      );
+      if (cmds_result.success && src[cmds_result.next.abs - p.abs] == "]") {
+        return {
+          success: true,
+          next: count_line_and_col(
+            src.slice(p_next.abs - p.abs),
+            cmds_result.next,
+            cmds_result.next.abs - p_next.abs + 1
+          ),
+          result: {
+            type: "with-cmds",
+            args,
+            name: name[1],
+            cmds: cmds_result.result,
+          },
+        };
+      }
     }
   }
   return {
