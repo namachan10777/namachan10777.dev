2dbb91044add0a04edb224ced9ca9f258a231547
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-09-11 10:16:41 +0900

    tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { AppProps } from "next/app";
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
