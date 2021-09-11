3083948f8852c373e7ff63881028610bd02e1d82
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-09-11 10:17:03 +0900

    fix React warnings

--- components/md.tsx
+++ components/md.tsx
@@ -6,23 +6,66 @@ export type Props = {
   mdast: MdAst.Root;
 };
 
+function calcHash(ast: Unist.Node) {
+  switch (ast.type) {
+    case "heading": {
+      const heading = ast as MdAst.Heading;
+      return `<h${heading.depth}>${heading.children.map(calcHash)}</h>`;
+    }
+    case "text": {
+      const text = ast as MdAst.Text;
+      return text.value;
+    }
+    case "paragraph": {
+      const paragraph = ast as MdAst.Paragraph;
+      return `<p>${paragraph.children.map(calcHash)}</p>`;
+    }
+    default:
+      throw `unsupported ast type ${ast.type}`;
+  }
+}
+
 function constructDom(ast: Unist.Node) {
   switch (ast.type) {
     case "heading": {
       const heading = ast as MdAst.Heading;
       switch (heading.depth) {
         case 1:
-          return <h1>{heading.children.map((c) => constructDom(c))}</h1>;
+          return (
+            <h1 key={calcHash(ast)}>
+              {heading.children.map((c) => constructDom(c))}
+            </h1>
+          );
         case 2:
-          return <h1>{heading.children.map((c) => constructDom(c))}</h1>;
+          return (
+            <h2 key={calcHash(ast)}>
+              {heading.children.map((c) => constructDom(c))}
+            </h2>
+          );
         case 3:
-          return <h1>{heading.children.map((c) => constructDom(c))}</h1>;
+          return (
+            <h3 key={calcHash(ast)}>
+              {heading.children.map((c) => constructDom(c))}
+            </h3>
+          );
         case 4:
-          return <h1>{heading.children.map((c) => constructDom(c))}</h1>;
+          return (
+            <h4 key={calcHash(ast)}>
+              {heading.children.map((c) => constructDom(c))}
+            </h4>
+          );
         case 5:
-          return <h1>{heading.children.map((c) => constructDom(c))}</h1>;
+          return (
+            <h5 key={calcHash(ast)}>
+              {heading.children.map((c) => constructDom(c))}
+            </h5>
+          );
         case 6:
-          return <h1>{heading.children.map((c) => constructDom(c))}</h1>;
+          return (
+            <h6 key={calcHash(ast)}>
+              {heading.children.map((c) => constructDom(c))}
+            </h6>
+          );
       }
       break;
     }
@@ -32,10 +75,12 @@ function constructDom(ast: Unist.Node) {
     }
     case "paragraph": {
       const paragraph = ast as MdAst.Paragraph;
-      return <p>{paragraph.children.map(constructDom)}</p>;
+      return <p key={calcHash(ast)}>{paragraph.children.map(constructDom)}</p>;
     }
+    case "toml":
+      return null;
     default:
-      return <div>UNSUPPORTED TYPE {ast.type}</div>;
+      return <div key={calcHash(ast)}>UNSUPPORTED TYPE {ast.type}</div>;
   }
 }
 const Md: React.FC<Props> = (props: Props) => {
