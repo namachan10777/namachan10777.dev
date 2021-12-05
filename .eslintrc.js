5fc298204011f334546cb05f7bab0f6c2ee0744a
Masaki Nakano <admin@namachan10777.dev>
2021-12-05 10:59:37 +0800

    format

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,42 +2,39 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'plugin:@next/next/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'standard',
-    'prettier'
+    "plugin:react/recommended",
+    "plugin:@next/next/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "standard",
+    "prettier",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  plugins: ["react", "@typescript-eslint"],
   settings: {
     react: {
-      version: "detect"
+      version: "detect",
     },
-    'import/parsers': {
-      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', 'd.ts'],
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", "d.ts"],
     },
-    'import/resolver': {
-      [require.resolve('eslint-import-resolver-node')]: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      }
+    "import/resolver": {
+      [require.resolve("eslint-import-resolver-node")]: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
     },
   },
   rules: {
     "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"]
-  }
-}
+    "@typescript-eslint/no-use-before-define": ["error"],
+  },
+};
