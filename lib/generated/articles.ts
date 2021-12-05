c0dc94d0597e424fbf1a7c09490c75774b690c35
Masaki Nakano <admin@namachan10777.dev>
2021-12-05 10:40:25 +0800

    track lib/generated/

---
+++ lib/generated/articles.ts
@@ -0,0 +1,14 @@
+/* eslint camelcase: 0 */
+/* eslint import/first: 0 */
+import articles_blog_blog_on_nextjs from '../../articles/blog/blog-on-nextjs.md';
+import articles_blog_cigarettes from '../../articles/blog/cigarettes.md';
+import articles_blog_new_home_network_2021 from '../../articles/blog/new-home-network-2021.md';
+export const blogs = [articles_blog_blog_on_nextjs,articles_blog_cigarettes,articles_blog_new_home_network_2021];
+
+import articles_diary_2021_09_14 from '../../articles/diary/2021-09-14.md';
+import articles_diary_2021_09_21 from '../../articles/diary/2021-09-21.md';
+import articles_diary_2021_11_04 from '../../articles/diary/2021-11-04.md';
+export const diaries = [articles_diary_2021_09_14,articles_diary_2021_09_21,articles_diary_2021_11_04];
+
+import articles_index from '../../articles/index.md';
+export const index = articles_index;
