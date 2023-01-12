f1b778cced8be8b13aae479ac01a27d494472c7c
Masaki Nakano <admin@namachan10777.dev>
2023-01-12 14:05:30 +0900

    pulldown-cmark

--- unmark/src/main.rs
+++ unmark/src/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
 use std::path::PathBuf;
+use tokio::fs;
+use tracing_subscriber::{fmt, EnvFilter};
 
 #[derive(Parser)]
 struct Opts {
@@ -8,7 +10,18 @@ struct Opts {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    use tracing_subscriber::prelude::*;
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env())
+        .with(fmt::layer())
+        .init();
     let opts = Opts::parse();
-    println!("Hello, world!");
+    let mut options = pulldown_cmark::Options::empty();
+    options.insert(pulldown_cmark::Options::ENABLE_STRIKETHROUGH);
+    let src = fs::read_to_string(opts.src).await?;
+    let parser = pulldown_cmark::Parser::new_ext(&src, options);
+    let mut html_output = String::new();
+    pulldown_cmark::html::push_html(&mut html_output, parser);
+    println!("{}", html_output);
     Ok(())
 }
