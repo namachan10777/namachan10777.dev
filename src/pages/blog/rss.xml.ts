2335f655569e94900c410372ce83d9ba2d37b99a
Masaki Nakano <admin@namachan10777.dev>
2024-03-10 18:39:04 +0900

    RSS

---
+++ src/pages/blog/rss.xml.ts
@@ -0,0 +1,21 @@
+import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+
+const blog = await getCollection("blog");
+
+export const GET: APIRoute = async (ctx) => {
+  return rss({
+    title: "namachan10777 Blog",
+    description: "分散システム、ストレージ、Web、あとそのほか",
+    site: ctx.site || "https://www.namachan10777.dev",
+    items: blog
+      .sort((a, b) => a.data.date.getDate() - b.data.date.getDate())
+      .map((blog) => ({
+        title: blog.data.title,
+        pubDate: blog.data.date,
+        description: blog.data.description,
+        link: `/blog/${blog.slug}`,
+      })),
+  });
+};
