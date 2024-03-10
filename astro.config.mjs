183b3ec9be5dab5c25e4e24d3516e07d99f7d982
Masaki Nakano <admin@namachan10777.dev>
2024-03-11 02:23:40 +0900

    remark-sectionize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import remarkSectionize from "remark-sectionize";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.namachan10777.dev",
   integrations: [icon(), react()],
+  markdown: {
+    remarkPlugins: [remarkSectionize],
+  },
 });
