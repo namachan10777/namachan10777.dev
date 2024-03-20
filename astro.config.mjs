99e0c8aa0b5913c06e73a8b379888574158e3e5f
Masaki Nakano <admin@namachan10777.dev>
2024-03-21 02:17:25 +0900

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import remarkSectionize from "remark-sectionize";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.namachan10777.dev",
-  integrations: [icon(), react()],
+  integrations: [icon(), react(), sitemap()],
   markdown: {
     remarkPlugins: [remarkSectionize],
   },
