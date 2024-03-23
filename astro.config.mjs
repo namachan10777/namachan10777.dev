e33aebfe09e835906e25ec83305e220a63eb8804
Masaki Nakano <admin@namachan10777.dev>
2024-03-23 10:23:00 +0900

    with copy-button

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import remarkSectionize from "remark-sectionize";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.namachan10777.dev",
-  integrations: [icon(), react(), sitemap(), tailwind()],
+  integrations: [icon(), react(), sitemap(), tailwind(), mdx()],
   markdown: {
     remarkPlugins: [remarkSectionize],
   },
