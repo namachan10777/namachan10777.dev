ce8cad7974772aac35c65186e530c74ffa261c04
Masaki Nakano <admin@namachan10777.dev>
2024-04-01 21:07:16 +0900

    enable qwik

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import remarkSectionize from "remark-sectionize";
 
+import qwikdev from "@qwikdev/astro";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.namachan10777.dev",
-  integrations: [icon(), react(), sitemap(), tailwind(), mdx()],
+  integrations: [icon(), sitemap(), tailwind(), mdx(), qwikdev()],
   markdown: {
     remarkPlugins: [remarkSectionize],
   },
