b55734e5aa700984e67147c094dd2640323a406a
Masaki Nakano <admin@namachan10777.dev>
2024-04-02 15:14:41 +0900

    success build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,22 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import remarkSectionize from "remark-sectionize";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.namachan10777.dev",
-  integrations: [icon(), sitemap(), tailwind(), mdx(), solidJs(), react()],
+  integrations: [icon(), sitemap(), tailwind(), mdx()],
   markdown: {
-    remarkPlugins: [remarkSectionize]
+    remarkPlugins: [remarkSectionize],
   },
   vite: {
     build: {
       rollupOptions: {
-        external: ["/pagefind/pagefind.js?url"]
-      }
-    }
-  }
+        external: ["/pagefind/pagefind.js?url"],
+      },
+    },
+  },
 });
