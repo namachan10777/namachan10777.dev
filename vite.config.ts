c68b8c1de63842899a9e7b8208ac0f04fcc8ba1a
Masaki.Nakano <admin@namachan10777.dev>
2024-04-06 01:11:28 +0900

    remove unused package from package.json

--- vite.config.ts
+++ vite.config.ts
@@ -35,15 +35,15 @@ export default defineConfig(({ command, mode }): UserConfig => {
     ssr:
       command === "build" && mode === "production"
         ? {
-            // All dev dependencies should be bundled in the server build
-            noExternal: Object.keys(devDependencies),
-            // Anything marked as a dependency will not be bundled
-            // These should only be production binary deps (including deps of deps), CLI deps, and their module graph
-            // If a dep-of-dep needs to be external, add it here
-            // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
-            // external: [...Object.keys(dependencies), 'bcrypt']
-            external: Object.keys(dependencies),
-          }
+          // All dev dependencies should be bundled in the server build
+          noExternal: Object.keys(devDependencies),
+          // Anything marked as a dependency will not be bundled
+          // These should only be production binary deps (including deps of deps), CLI deps, and their module graph
+          // If a dep-of-dep needs to be external, add it here
+          // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
+          // external: [...Object.keys(dependencies), 'bcrypt']
+          external: Object.keys(dependencies),
+        }
         : undefined,
     server: {
       headers: {
