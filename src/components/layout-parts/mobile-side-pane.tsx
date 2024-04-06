02373fea809bfcdefac2b2d99438d1b5c644f30e
Masaki.Nakano <admin@namachan10777.dev>
2024-04-07 02:17:21 +0900

    styled nav

--- src/components/layout-parts/mobile-side-pane.tsx
+++ src/components/layout-parts/mobile-side-pane.tsx
@@ -1,42 +1,11 @@
 import { component$ } from "@builder.io/qwik";
-import { NavItem, SubNavItem, navItems } from "./nav-menu";
-import Icon from "~/assets/icon.webp?jsx";
-
-const SubMenu = (props: { items: SubNavItem[] }) => {
-  return (
-    <ul class="contents">
-      {props.items.map((item) => (
-        <li class="col-start-2 text-lg underline" key={item.href}>
-          <a href={item.href}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-const Item = (props: { item: NavItem }) => {
-  return (
-    <li class="col-span-2 grid grid-cols-[subgrid] items-center gap-2 border-t border-black py-4">
-      <a href={props.item.href} class="contents">
-        <props.item.icon class="text-xl" />
-        <span class="text-xl font-bold underline">{props.item.title}</span>
-      </a>
-      {props.item.submenu ? <SubMenu items={props.item.submenu} /> : null}
-    </li>
-  );
-};
+import NavLinks from "./nav-links";
+import { navItems } from "./nav-menu";
 
 export default component$(() => {
   return (
     <div class="h-full border-l border-black bg-white p-4">
-      <nav class="grid w-full  grid-cols-[2rem_1fr] flex-col">
-        <h3 class="col-start-2 pb-4 text-xl font-bold text-gray-600">Links</h3>
-        <ul class="contents">
-          {navItems.map((item) => (
-            <Item key={item.href} item={item} />
-          ))}
-        </ul>
-      </nav>
+      <NavLinks items={navItems} title="Links" />
     </div>
   );
 });
---
+++ src/components/layout-parts/nav-links.tsx
@@ -0,0 +1,52 @@
+import { component$ } from "@builder.io/qwik";
+import { NavItem, SubNavItem, navItems } from "./nav-menu";
+
+export type Props = {
+  items: NavItem[];
+  title?: string;
+};
+
+const SubMenu = (props: { items: SubNavItem[] }) => {
+  return (
+    <ul class="contents">
+      {props.items.map((item) => (
+        <li class="col-start-2 text-lg underline" key={item.href}>
+          <a href={item.href}>{item.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const Item = (props: { item: NavItem }) => {
+  return (
+    <li class="col-span-2 grid grid-cols-[subgrid] items-center gap-1 border-t border-black py-4 first:border-none">
+      <a href={props.item.href} class="contents">
+        <props.item.icon class="text-xl" />
+        <span class="text-xl font-bold underline">{props.item.title}</span>
+      </a>
+      {props.item.submenu ? <SubMenu items={props.item.submenu} /> : null}
+    </li>
+  );
+};
+
+export default (props: Props) => {
+  return (
+    <section class="grid w-full grid-cols-[2rem_1fr] flex-col">
+      <nav class="contents">
+        <ul class="contents">
+          <li>
+            {props.title ? (
+              <h2 class="col-start-2 py-4 text-xl font-bold text-gray-600">
+                {props.title}
+              </h2>
+            ) : null}
+          </li>
+          {props.items.map((item) => (
+            <Item key={item.href} item={item} />
+          ))}
+        </ul>
+      </nav>
+    </section>
+  );
+};
