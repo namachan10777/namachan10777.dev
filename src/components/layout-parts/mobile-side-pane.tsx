9e4aaa5e4f18cf61d8a5e7de792e0639f9a5845a
Masaki.Nakano <admin@namachan10777.dev>
2024-04-07 01:53:57 +0900

    layout mobile nav

--- src/components/layout-parts/mobile-side-pane.tsx
+++ src/components/layout-parts/mobile-side-pane.tsx
@@ -1,22 +1,42 @@
 import { component$ } from "@builder.io/qwik";
-import { navItems } from "./nav-menu";
+import { NavItem, SubNavItem, navItems } from "./nav-menu";
+import Icon from "~/assets/icon.webp?jsx";
+
+const SubMenu = (props: { items: SubNavItem[] }) => {
+  return (
+    <ul class="contents">
+      {props.items.map((item) => (
+        <li class="col-start-2 text-lg underline" key={item.href}>
+          <a href={item.href}>{item.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const Item = (props: { item: NavItem }) => {
+  return (
+    <li class="col-span-2 grid grid-cols-[subgrid] items-center gap-2 border-t border-black py-4">
+      <a href={props.item.href} class="contents">
+        <props.item.icon class="text-xl" />
+        <span class="text-xl font-bold underline">{props.item.title}</span>
+      </a>
+      {props.item.submenu ? <SubMenu items={props.item.submenu} /> : null}
+    </li>
+  );
+};
 
 export default component$(() => {
   return (
-    <nav class="flex h-full w-full flex-col border-l border-black bg-white">
-      <ul>
-        {navItems.map((item) => (
-          <li key={item.href}>
-            <a
-              href={item.href}
-              class="flex flex-row items-center gap-2 text-xl"
-            >
-              <item.icon />
-              <span>{item.title}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div class="h-full border-l border-black bg-white p-4">
+      <nav class="grid w-full  grid-cols-[2rem_1fr] flex-col">
+        <h3 class="col-start-2 pb-4 text-xl font-bold text-gray-600">Links</h3>
+        <ul class="contents">
+          {navItems.map((item) => (
+            <Item key={item.href} item={item} />
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 });
--- src/components/layout-parts/nav-menu.ts
+++ src/components/layout-parts/nav-menu.ts
@@ -1,15 +1,41 @@
 import type { SVGProps, JSXNode } from "@builder.io/qwik";
 import { InNotes, InGithub, InTwitter } from "@qwikest/icons/iconoir";
+import { allBlogs } from "content-collections";
+
+export type SubNavItem = {
+  title: string;
+  href: string;
+};
 
 export type NavItem = {
   title: string;
   href: string;
   icon: (props: SVGProps<SVGSVGElement>) => JSXNode<unknown>;
+  submenu?: SubNavItem[];
 };
 
+const blogSubmenu: SubNavItem[] = allBlogs
+  .sort((a, b) => Date.parse(b.date) - Date.parse(a.date))
+  .map((blog) => ({ title: blog.title, href: `/blog/${blog._meta.fileName}` }))
+  .slice(0, Math.min(3, allBlogs.length));
+
 export const navItems: NavItem[] = [
-  { title: "Blog", href: "/blog", icon: InNotes },
-  { title: "GitHub", href: "https://github.com/namachan10777", icon: InGithub },
+  { title: "Blog", href: "/blog", icon: InNotes, submenu: blogSubmenu },
+  {
+    title: "GitHub",
+    href: "https://github.com/namachan10777",
+    icon: InGithub,
+    submenu: [
+      {
+        title: "namahchan10777.dev",
+        href: "https://github.com/namachan10777.dev",
+      },
+      {
+        title: "ekika",
+        href: "https://github.com/ekika",
+      },
+    ],
+  },
   {
     title: "Twitter",
     href: "https://github.com/namachan10777",
