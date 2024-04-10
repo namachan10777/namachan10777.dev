04442491c74b194d39405c685a48ca6a12b1a014
Masaki.Nakano <admin@namachan10777.dev>
2024-04-10 23:42:54 +0900

    bug ...?

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -6,7 +6,7 @@ import {
   useTask$,
   useVisibleTask$,
 } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
+import { routeLoader$, useLocation } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 import Header from "~/components/layout-parts/header";
 import MobileSidePane from "~/components/layout-parts/mobile-side-pane";
@@ -35,6 +35,7 @@ export const useServerTimeLoader = routeLoader$(() => {
 export default component$(() => {
   const sidePaneOpen = useSignal(false);
   const showSearchDialog = useSignal(false);
+  const location = useLocation();
   const scrollY = useSignal(0);
   useStylesScoped$(style);
   const freezeMainContent = sidePaneOpen.value || showSearchDialog.value;
@@ -47,6 +48,12 @@ export default component$(() => {
       }
     }
   });
+  useVisibleTask$(({ track }) => {
+    track(() => location.url);
+    sidePaneOpen.value = false;
+    showSearchDialog.value = false;
+  });
+
   // eslint-disable-next-line qwik/no-use-visible-task
   useVisibleTask$(({ track }) => {
     track(() => sidePaneOpen.value);
