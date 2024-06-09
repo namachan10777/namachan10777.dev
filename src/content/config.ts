383bd4ca6c776875ad3d6c4b0029cf2f9717c914
Masaki.Nakano <admin@namachan10777.dev>
2024-06-09 19:56:23 +0900

    events

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from "astro:content";
 
-const postCollection = defineCollection({
+const post = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
@@ -11,6 +11,26 @@ const postCollection = defineCollection({
   }),
 });
 
+const pub = defineCollection({
+  type: "data",
+  schema: z.object({
+    title: z.string(),
+    booktitle: z.string(),
+    href: z.string().nullish(),
+    year: z.number(),
+  }),
+});
+
+const event = defineCollection({
+  type: "data",
+  schema: z.object({
+    date: z.date(),
+    title: z.string(),
+  }),
+});
+
 export const collections = {
-  post: postCollection,
+  post,
+  pub,
+  event,
 };
