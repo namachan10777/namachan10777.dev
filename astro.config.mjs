77fe710901023e1d5dff6d10c0d9f15adb0b191d
Masaki.Nakano <admin@namachan10777.dev>
2024-06-10 02:13:42 +0900

    styled

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,16 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tsConfigPaths from "vite-tsconfig-paths";
-
 import tailwind from "@astrojs/tailwind";
+import remarkSectionize from "remark-sectionize";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.namachan10777.dev",
   integrations: [solidJs(), sitemap(), mdx(), pagefind(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkSectionize],
+  },
   vite: {
     plugins: [tsConfigPaths()],
   },
