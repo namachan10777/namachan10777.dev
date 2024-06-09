572fbb955fd55bcbc48e441914b3edfe1fc179b6
Masaki.Nakano <admin@namachan10777.dev>
2024-06-09 22:04:50 +0900

    install pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import pagefind from "astro-pagefind";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), sitemap(), mdx()],
+  site: "https://www.namachan10777.dev",
+  integrations: [solidJs(), sitemap(), mdx(), pagefind()],
   vite: {
     plugins: [tsConfigPaths()],
   },
