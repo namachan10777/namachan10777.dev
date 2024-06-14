74840f2da4614270ac0f95ba9e5346b4f0fbe304
Masaki.Nakano <admin@namachan10777.dev>
2024-06-15 01:41:07 +0900

    slim dependency

--- src/lib/global-topic.ts
+++
@@ -1,5 +0,0 @@
-import { Topics } from "./topic";
-import { defineTopic } from "./topic/types";
-
-export const topics = new Topics("topics-global");
-export const menuTopicDef = defineTopic(false);
