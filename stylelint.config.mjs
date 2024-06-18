03c5276013921e75a7f6c4d109330bb9b47e7c09
Masaki.Nakano <admin@namachan10777.dev>
2024-06-18 20:52:16 +0900

    stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ["stylelint-config-html/astro", "stylelint-config-standard"],
+  rules: {
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global"],
+      },
+    ],
+  },
+  ignoreFiles: [
+    "node_modules/**",
+    "dist/**",
+    ".astro/**",
+    "src/layouts/katex.css",
+  ],
+};
