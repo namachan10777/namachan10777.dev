b2d51e2d60fa85c3fbb677d7f662db1d03f9f35d
Masaki.Nakano <admin@namachan10777.dev>
2024-06-22 11:55:47 +0900

    theme-switcher

---
+++ src/lib/theme.ts
@@ -0,0 +1,20 @@
+export type Theme = "light" | "dark";
+
+export function sanitizeTheme(theme?: string): Theme {
+  switch (theme) {
+    case "light":
+    case "dark":
+      return theme;
+    default:
+      return "light";
+  }
+}
+
+export function cycleTheme(theme: Theme): Theme {
+  switch (theme) {
+    case "dark":
+      return "light";
+    case "light":
+      return "dark";
+  }
+}
