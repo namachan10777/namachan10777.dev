79c41b469a299d26b4a63c135aae7edda424154e
Masaki.Nakano <admin@namachan10777.dev>
2024-06-24 04:10:22 +0900

    refactor theme switcher

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,30 +1,15 @@
 export type Theme = "light" | "dark";
 
-export function sanitizeTheme(theme?: string): Theme {
-  switch (theme) {
-    case "light":
-    case "dark":
-      return theme;
-    default:
-      return "light";
-  }
-}
-
-export function cycleTheme(theme: Theme): Theme {
-  switch (theme) {
-    case "dark":
-      return "light";
-    case "light":
-      return "dark";
-  }
-}
-
-export const giscusTheme: { [key in Theme]: string } = {
-  light: "light",
-  dark: "dark",
-};
-
 export const ariaLabel: { [key in Theme]: string } = {
   dark: "ライトモードにする",
   light: "ダークモードにする",
 };
+
+export interface ThemeProvider {
+  register(themeAdapter: ThemeAdapter): void;
+}
+
+export interface ThemeAdapter {
+  init(theme: Theme): void;
+  apply(theme: Theme): void;
+}
