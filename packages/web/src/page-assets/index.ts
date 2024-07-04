e89a1136e096ed5a45040c6eae6966af9b231e81
Masaki.Nakano <admin@namachan10777.dev>
2024-07-05 02:18:19 +0900

    mdx index page

---
+++ packages/web/src/page-assets/index.ts
@@ -0,0 +1,29 @@
+import { getCollection } from "astro:content";
+
+export const events = (await getCollection("event")).sort(
+  (a, b) => b.data.date.getTime() - a.data.date.getTime(),
+);
+export const posts = (
+  await getCollection("post", (post) => post.data.publish)
+).sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
+export const pubs = (await getCollection("pub")).sort(
+  (a, b) => b.data.date.getTime() - a.data.date.getTime(),
+);
+
+export const links = [
+  {
+    text: "GitHub",
+    href: "https://github.com/namachan10777",
+    icon: "iconoir:github",
+  },
+  {
+    text: "X",
+    href: "https://x.com/namachan10777",
+    icon: "iconoir:x",
+  },
+  {
+    text: "Post",
+    href: "/post/page/1",
+    icon: "iconoir:post",
+  },
+];
