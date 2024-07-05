1f15a652a4e04118c6e4351baeb41ec7d7c90d2b
Masaki Nakano <namachan@MasakinoMacBook-Pro.local>
2024-07-06 02:22:37 +0900

    pagefind package

--- packages/pagefind-api/index.ts
+++ packages/pagefind-api/index.ts
@@ -20,3 +20,11 @@ export interface Pagefind {
   init: () => Promise<void>;
   debouncedSearch: (query: string) => Promise<Found | null>;
 }
+
+export async function loadPagefind(path: string): Promise<Pagefind> {
+  const mod = await import(
+    /* @vite-ignore */
+    `${path}`
+  );
+  return mod;
+}
