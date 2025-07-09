c243b27def55a29f58a0ff08714a00c84fd7c798
Masaki.Nakano <admin@namachan10777.dev>
2025-07-10 01:56:39 +0900

    reset

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,31 +1,18 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
-// Define the schema for the post collection
-const postCollection = defineCollection({
-  type: 'content',
-  schema: ({ image }) =>
-    z.object({
-      tags: z.array(z.string()).optional(),
-      date: z.date(),
-      description: z.string(),
-      title: z.string(),
-      publish: z.boolean().default(true),
-      og_image: image().optional(), // OGP画像用の背景画像のパス
-    }),
-});
-
-const publicationCollection = defineCollection({
-  type: 'data',
+const post = defineCollection({
+  loader: glob({
+    pattern: "**/*.mdx",
+    base: "./content/post",
+  }),
   schema: z.object({
+    tags: z.array(z.string()),
     title: z.string(),
+    description: z.string(),
+    publish: z.boolean(),
     date: z.date(),
-    booktitle: z.string(),
-    href: z.string().optional(),
   }),
 });
 
-// Export the collections
-export const collections = {
-  post: postCollection,
-  pub: publicationCollection,
-};
+export const collections = { post };
