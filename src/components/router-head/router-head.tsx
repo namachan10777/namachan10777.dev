9f843f5759be6c762f1a14ec049456bcf00ac108
Masaki.Nakano <admin@namachan10777.dev>
2025-07-16 00:54:33 +0900

    KaTeX

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -15,6 +15,12 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico" />
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.css"
+        integrity="sha384-5TcZemv2l/9On385z///+d7MSYlvIEw9FuZTIdZ14vJLqWphw7e7ZPuOiCHJcFCP"
+        crossOrigin="anonymous"
+      />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
