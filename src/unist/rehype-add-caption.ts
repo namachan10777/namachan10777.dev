8f046fa6cb684ae9172aed9f0acb3425fd54952d
Masaki.Nakano <admin@namachan10777.dev>
2025-07-16 01:19:38 +0900

    fix image conversion

--- src/unist/rehype-add-caption.ts
+++ src/unist/rehype-add-caption.ts
@@ -1,20 +1,42 @@
 import type * as hast from "hast";
+import { MdxJsxFlowElement } from "mdast-util-mdx";
+import { MdxJsxFlowElementHast } from "mdast-util-mdx-jsx";
 import { visit } from "unist-util-visit";
+
+function foldNonFigureContent(stack: hast.ElementContent[]) {
+  const children: hast.ElementContent[] = [];
+  while (true) {
+    const last = stack.pop();
+    if (last === undefined) {
+      break;
+    }
+    if (last.type === "element" && last.tagName == "figure") {
+      stack.push(last);
+      break;
+    }
+  }
+  if (children.length > 0) {
+    stack.push({
+      type: "element",
+      tagName: "p",
+      properties: {},
+      children,
+    });
+  }
+}
 
 function editFigureParagraph(paragraph: hast.Element) {
   paragraph.tagName = "div";
   const children: hast.ElementContent[] = [];
   for (const child of paragraph.children) {
-    if (
-      child.type === "mdxJsxFlowElement" &&
-      child.data &&
-      "type" in child.data &&
-      child.data.type === "inserted-image"
-    ) {
+    if (isInsertedImage(child)) {
       const metaRef = child as unknown as {
         title?: string;
         alt: string;
       };
+
+      foldNonFigureContent(children);
+
       children.push({
         type: "element",
         tagName: "figure",
@@ -38,13 +60,28 @@ function editFigureParagraph(paragraph: hast.Element) {
       children.push(child);
     }
   }
+  foldNonFigureContent(children);
   paragraph.children = children;
+}
+
+function isInsertedImage(
+  node: hast.ElementContent,
+): node is MdxJsxFlowElementHast {
+  return (
+    node.type === "mdxJsxFlowElement" &&
+    node.data !== undefined &&
+    "type" in node.data &&
+    node.data.type === "inserted-image"
+  );
 }
 
 export default function rehypeAddCaptions() {
   return (tree: hast.Root) => {
     visit(tree, "element", (node) => {
-      if (node.tagName === "p" && node.children.length > 0) {
+      if (
+        node.tagName === "p" &&
+        node.children.filter(isInsertedImage).length > 0
+      ) {
         editFigureParagraph(node);
       }
     });
