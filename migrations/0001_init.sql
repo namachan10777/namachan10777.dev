4af00189315e7aa59dc3f47da6fc3a67585453b0
Masaki.Nakano <admin@namachan10777.dev>
2025-09-04 03:39:44 +0900

    update config

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -1,25 +1,42 @@
--- Migration number: 0001 	 2025-08-23T17:06:43.806Z
-CREATE TABLE IF NOT EXISTS posts(
-    id TEXT NOT NULL PRIMARY KEY,
-    body TEXT,
-    description TEXT NOT NULL,
-    publish BOOL,
-    og_image TEXT,
+-- Table: posts
+
+CREATE TABLE IF NOT EXISTS posts (
+    id TEXT NOT NULL,
+    body TEXT NOT NULL,
     title TEXT NOT NULL,
-    hash TEXT NOT NULL
+    description TEXT NOT NULL,
+    date TEXT NOT NULL,
+    publish INTEGER,
+    hash TEXT NOT NULL,
+    og_image TEXT,
+    PRIMARY KEY (id)
 );
+CREATE INDEX IF NOT EXISTS index_posts_id ON posts(id);
+CREATE INDEX IF NOT EXISTS index_posts_date ON posts(date(date));
+CREATE INDEX IF NOT EXISTS index_posts_publish ON posts(publish);
+CREATE INDEX IF NOT EXISTS index_posts_hash ON posts(hash);
+CREATE INDEX IF NOT EXISTS index_posts_body ON posts(body->>'hash');
+CREATE INDEX IF NOT EXISTS index_posts_og_image ON posts(og_image->>'hash');
 
 
-CREATE TABLE IF NOT EXISTS post_tags(
-    id TEXT NOT NULL,
-    tags TEXT NOT NULL,
-    PRIMARY KEY(id, tags),
-    FOREIGN KEY(id) REFERENCES posts(id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS post_tags (
+    post_id TEXT NOT NULL,
+    tag TEXT NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    PRIMARY KEY (post_id, tag)
 );
+CREATE INDEX IF NOT EXISTS index_post_tags_tag ON post_tags(tag);
 
-CREATE TABLE IF NOT EXISTS post_images(
-    id TEXT NOT NULL,
-    image TEXT NOT NULL,
-    PRIMARY KEY(id, image),
-    FOREIGN KEY(id) REFERENCES posts(id) ON DELETE CASCADE
+
+CREATE TABLE IF NOT EXISTS post_images (
+    post_id TEXT NOT NULL,
+    src_id TEXT NOT NULL,
+    image TEXT,
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    PRIMARY KEY (post_id, src_id)
 );
+CREATE INDEX IF NOT EXISTS index_post_images_src_id ON post_images(src_id);
+CREATE INDEX IF NOT EXISTS index_post_images_image ON post_images(image->>'hash');
+
+
+
