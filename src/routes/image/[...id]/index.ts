0ee10937f5e12c6fec0dc5f953f33cd30adec773
Masaki.Nakano <admin@namachan10777.dev>
2025-09-06 02:52:13 +0900

    fix image

--- src/routes/image/[...id]/index.ts
+++ src/routes/image/[...id]/index.ts
@@ -1,49 +1,58 @@
 import { RequestHandler } from "@builder.io/qwik-city";
+import * as v from "valibot";
 
 const allowedWidths = new Set([300, 500, 800, 1000]);
-const allowedFormats = new Set(["avif", "webp"]);
+const formatValidator = v.union([
+  v.literal("avif"),
+  v.literal("webp"),
+  v.literal("json"),
+  v.literal("jpeg"),
+  v.literal("png"),
+  v.literal("baseline-jpeg"),
+  v.literal("png-force"),
+  v.literal("svg"),
+]);
+const widthValidator = v.union([
+  v.literal(300),
+  v.literal(500),
+  v.literal(800),
+  v.literal(1000),
+]);
 
-function parseUrl(url: URL): [string, number, string] {
+function parseUrl(
+  url: URL,
+): [
+  string,
+  v.InferOutput<typeof widthValidator>,
+  v.InferOutput<typeof formatValidator>,
+] {
   const width = parseInt(url.searchParams.get("width")!, 10);
   const format = url.searchParams.get("format")!;
-  if (!allowedWidths.has(width) || !allowedFormats.has(format)) {
+  if (!allowedWidths.has(width)) {
     throw new Error("Invalid width or format");
   }
-  return [url.pathname.substring(1), width, format];
+  return [
+    url.pathname.substring(1),
+    v.parse(widthValidator, width),
+    v.parse(formatValidator, format),
+  ];
 }
 
 export const onGet: RequestHandler = async ({ request, send }) => {
   try {
-    const cache = await caches.open("namachan10777dev:image");
     const url = new URL(request.url);
     const [key, width, format] = parseUrl(url);
-    const cachedResponse = await cache.match(request);
-    if (cachedResponse) {
-      send(cachedResponse);
-      return;
-    }
-
-    const imageResponse = await fetch(
-      `https://assets.namachan10777.dev/${key}`,
-      {
-        cf: {
+    const response = await fetch(`https://assets.namachan10777.dev/${key}`, {
+      cf: {
+        image: {
           format,
           width,
           fit: "contain",
         },
-      },
-    );
-
-    if (imageResponse.status != 200) {
-      send(imageResponse.status, "");
-      return;
-    }
-    const response = new Response(imageResponse.body, {
-      headers: {
-        "Cache-Control": "public, max-age=31536000",
+        cacheEverything: true,
+        cacheTtl: 60,
       },
     });
-    await cache.put(request, response.clone());
     send(response);
   } catch (error) {
     console.warn(error);
