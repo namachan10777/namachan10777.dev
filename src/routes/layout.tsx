5367f96061b0a1718a84bbfb915341b461302307
Masaki.Nakano <admin@namachan10777.dev>
2025-12-09 02:50:21 +0900

    feat: ページ遷移時のナビゲーションプログレスバーを追加

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,11 +1,13 @@
 import { Slot, component$ } from "@builder.io/qwik";
 import { Header } from "~/components/header";
 import { Footer } from "~/components/footer";
+import { NavigationProgress } from "~/components/navigation-progress";
 import styles from "./layout.module.css";
 
 export default component$(() => {
   return (
     <div class={styles.container}>
+      <NavigationProgress />
       <Header />
       <main class={styles.main}>
         <div class={styles.content}>
