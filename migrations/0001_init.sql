1994908f5d317cdc5c633ffb4febf67b977b5fd9
Masaki.Nakano <admin@namachan10777.dev>
2026-05-01 13:54:33 +0900

    add sql linting

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -5,35 +5,37 @@ CREATE TABLE IF NOT EXISTS posts (
     body TEXT NOT NULL,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
-    date TEXT NOT NULL,
+    "date" TEXT NOT NULL,
     publish INTEGER,
     hash TEXT NOT NULL,
     og_image TEXT,
     PRIMARY KEY (id)
 );
-CREATE INDEX IF NOT EXISTS index_posts_id ON posts(id);
-CREATE INDEX IF NOT EXISTS index_posts_date ON posts(date(date));
-CREATE INDEX IF NOT EXISTS index_posts_publish ON posts(publish);
-CREATE INDEX IF NOT EXISTS index_posts_hash ON posts(hash);
-CREATE INDEX IF NOT EXISTS index_posts_body ON posts(body->>'hash');
-CREATE INDEX IF NOT EXISTS index_posts_og_image ON posts(og_image->>'hash');
+CREATE INDEX IF NOT EXISTS index_posts_id ON posts (id);
+CREATE INDEX IF NOT EXISTS index_posts_date ON posts (date("date"));
+CREATE INDEX IF NOT EXISTS index_posts_publish ON posts (publish);
+CREATE INDEX IF NOT EXISTS index_posts_hash ON posts (hash);
+CREATE INDEX IF NOT EXISTS index_posts_body ON posts (body ->> 'hash');
+CREATE INDEX IF NOT EXISTS index_posts_og_image ON posts (og_image ->> 'hash');
 
 
 CREATE TABLE IF NOT EXISTS post_tags (
     post_id TEXT NOT NULL,
     tag TEXT NOT NULL,
-    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE,
     PRIMARY KEY (post_id, tag)
 );
-CREATE INDEX IF NOT EXISTS index_post_tags_tag ON post_tags(tag);
+CREATE INDEX IF NOT EXISTS index_post_tags_tag ON post_tags (tag);
 
 
 CREATE TABLE IF NOT EXISTS post_images (
     post_id TEXT NOT NULL,
     src_id TEXT NOT NULL,
     image TEXT,
-    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE,
     PRIMARY KEY (post_id, src_id)
 );
-CREATE INDEX IF NOT EXISTS index_post_images_src_id ON post_images(src_id);
-CREATE INDEX IF NOT EXISTS index_post_images_image ON post_images(image->>'hash');
+CREATE INDEX IF NOT EXISTS index_post_images_src_id ON post_images (src_id);
+CREATE INDEX IF NOT EXISTS index_post_images_image ON post_images (
+    image ->> 'hash'
+);
--- migrations/0002_comment_good.sql
+++ migrations/0002_comment_good.sql
@@ -6,13 +6,13 @@ CREATE TABLE IF NOT EXISTS comments (
     created_at TEXT NOT NULL,
     name TEXT NOT NULL,
     content TEXT NOT NULL,
-    PRIMARY KEY(post_id, id),
-    FOREIGN KEY(post_id) REFERENCES posts(id) ON DELETE CASCADE
+    PRIMARY KEY (post_id, id),
+    FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS index_comments_create_at ON comments(created_at);
+CREATE INDEX IF NOT EXISTS index_comments_create_at ON comments (created_at);
 CREATE TABLE IF NOT EXISTS likes (
     post_id TEXT NOT NULL PRIMARY KEY,
     count INTEGER NOT NULL DEFAULT 0,
-    FOREIGN KEY(post_id) REFERENCES posts(id) ON DELETE CASCADE
+    FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE
 );
