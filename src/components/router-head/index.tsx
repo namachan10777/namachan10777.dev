8200bf3b2d8fe83b370dfa1b0445c178ddca2f4f
Masaki.Nakano <admin@namachan10777.dev>
2026-05-11 00:23:16 +0900

    self-hosted katex

--- src/components/router-head/index.tsx
+++ src/components/router-head/index.tsx
@@ -17,12 +17,7 @@ export const RouterHead = component$(() => {
         content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content"
       />
       <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico" />
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/katex@0.16.22/dist/katex.min.css"
-        integrity="sha384-5TcZemv2l/9On385z///+d7MSYlvIEw9FuZTIdZ14vJLqWphw7e7ZPuOiCHJcFCP"
-        crossOrigin="anonymous"
-      />
+      <link rel="stylesheet" href="/katex.min.css" crossOrigin="anonymous" />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
